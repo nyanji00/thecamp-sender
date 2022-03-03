afd413f5c2cabd1f4fc9cef125c1ff220508e11e
nyanji00 <wl77886@gmail.com>
2022-03-03 22:14:14 +0900

    initial setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider
+      resetCSS={true}
+      theme={extendTheme({ fonts: { body: "Spoqa Han Sans Neo, sans-serif" } })}
+    >
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
